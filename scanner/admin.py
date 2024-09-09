35bb8ed9abc6611659f41d4e62c92a170c5c6ccf
Descent <natibatian@gmail.com>
2024-09-10 01:00:28 +0700

    feat: add sercurity-alert to admin

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Target,Task, Crawler, TelegramUser
+from .models import Target,Task, Crawler, TelegramUser, SecurityAlert
 admin.site.register(Target)
 admin.site.register(Task)
 admin.site.register(Crawler)
 admin.site.register(TelegramUser)
+admin.site.register(SecurityAlert)
